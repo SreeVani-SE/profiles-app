21a137e8c8d85166ad201451b03b60b060476c33
Sree Vani <sjalagari@albany.edu>
2025-11-12 22:06:26 +0000

    Readme update 1

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/profiles-app/' // <-- set to '/<repo-name>/'
+  base: '/profiles-app/' 
 })
